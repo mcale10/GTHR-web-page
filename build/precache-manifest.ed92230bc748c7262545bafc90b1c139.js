d39b1efc96b3c978d928ff6347c42443a666cb5f
Marko Ćaleta <marko.caleta@gmail.com>
2021-12-28 12:00:40 +0100

    +

---
+++ build/precache-manifest.ed92230bc748c7262545bafc90b1c139.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2951fb8a30a31d93e188fc2b5610ff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9555c7a8f238da039277",
+    "url": "/static/css/main.78318061.chunk.css"
+  },
+  {
+    "revision": "59b890cae7994fd1a673",
+    "url": "/static/js/2.d4cc6a1c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d4cc6a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9555c7a8f238da039277",
+    "url": "/static/js/main.039a111e.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/static/media/img-8.d211c298.jpg"
+  }
+]);
