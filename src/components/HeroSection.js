981bdd1c570b1d79223aa31da459cd8b6091da24
Marko Ćaleta <marko.caleta@gmail.com>
2021-12-28 12:02:07 +0100

    +

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/gthr-intro.mp4' autoPlay loop muted />
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>GRAN TURISMO HRVATSKA</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
